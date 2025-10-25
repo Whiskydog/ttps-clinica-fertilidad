8dbb7001bbd1149088ec40ae1e0b2e698b5c977d
Alan Varde <alan.varde@gmail.com>
2025-10-24 22:48:58 -0300

    refactor: rename UserParam decorator to CurrentUser for sign-in method

--- apps/api/src/modules/auth/auth.controller.ts
+++ apps/api/src/modules/auth/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, HttpCode, HttpStatus, Post, Res, UseGuards } from "@nestjs/common";
 import { LocalAuthGuard } from "./guards/local-auth.guard";
-import { UserParam } from "@modules/users/decorators/user.decorator";
+import { CurrentUser } from "@modules/auth/decorators/current-user.decorator";
 import { User } from "@modules/users/entities/user.entity";
 import { AuthService } from "./auth.service";
 import type { Response } from "express";
@@ -14,7 +14,7 @@ export class AuthController {
   @UseGuards(LocalAuthGuard)
   @HttpCode(HttpStatus.OK)
   @Post('sign-in')
-  async signIn(@UserParam() user: User, @Res({ passthrough: true }) response: Response) {
+  async signIn(@CurrentUser() user: User, @Res({ passthrough: true }) response: Response) {
     const signInResult = await this.authService.signIn(user);
 
     if ('accessToken' in signInResult) {
--- apps/api/src/modules/auth/decorators/current-user.decorator.ts
+++ apps/api/src/modules/auth/decorators/current-user.decorator.ts
@@ -1,7 +1,7 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { AuthRequest } from '@common/interfaces/auth-request.interface';
 
-export const UserParam = createParamDecorator(
+export const CurrentUser = createParamDecorator(
   (_data: unknown, ctx: ExecutionContext) => {
     const request: AuthRequest = ctx.switchToHttp().getRequest();
     return request.user;
