89653ca37c6b456a51f926a67ef2ea86f1c7a4a1
Juan Manuel Vila <512juanm@gmail.com>
2025-11-21 01:00:05 -0300

    feat: integrar fenotipos con API externa del banco de gametos

--- apps/api/src/modules/scheduler/scheduler.module.ts
+++ apps/api/src/modules/scheduler/scheduler.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
 import { TreatmentSchedulerService } from './treatment-scheduler.service';
 import { TreatmentsModule } from '../treatments/treatments.module';
+import { Group10TelegramBotModule } from '@external/group10-telegram-bot/group10-telegram-bot.module';
+import { Group8NoticesModule } from '@external/group8-notices/group8-notices.module';
 
 @Module({
-  imports: [ScheduleModule.forRoot(), TreatmentsModule],
+  imports: [
+    ScheduleModule.forRoot(),
+    TreatmentsModule,
+    Group10TelegramBotModule,
+    Group8NoticesModule,
+  ],
   providers: [TreatmentSchedulerService],
 })
 export class SchedulerModule {}
