d324dc81141f0d6e761760bda8f90c24b353358d
Alan Varde <alan.varde@gmail.com>
2025-12-20 19:04:45 -0300

    feat: implement Payments module with controller and service for patient debt management

---
+++ apps/api/src/modules/payments/payments.service.ts
@@ -0,0 +1,49 @@
+import { getHttpExceptionFromAxiosError } from '@common/utils/errors.utils';
+import { HttpService } from '@nestjs/axios';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { ExternalPatientDebtResponse, PatientDebt } from '@repo/contracts';
+import { AxiosError } from 'axios';
+import { catchError, firstValueFrom, map, of } from 'rxjs';
+
+@Injectable()
+export class PaymentsService {
+  private readonly apiUrl: string;
+
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly httpService: HttpService,
+  ) {
+    this.apiUrl = this.configService.get<string>('PAYMENTS_API_URL');
+  }
+
+  async getOwnDebt(patientId: number): Promise<PatientDebt> {
+    const url = `${this.apiUrl}/deuda-paciente`;
+
+    const externalPatientDebtDetail = await firstValueFrom(
+      this.httpService
+        .post<ExternalPatientDebtResponse>(url, {
+          id_paciente: patientId,
+          numero_grupo: 7,
+        })
+        .pipe(
+          map((res) => res.data),
+          catchError((error: AxiosError) => {
+            if (error.status === 404) {
+              return of({
+                id_paciente: patientId,
+                numero_grupo: 7,
+                deuda_total: 0,
+              });
+            } else {
+              throw getHttpExceptionFromAxiosError(error);
+            }
+          }),
+        ),
+    );
+
+    return {
+      debt: externalPatientDebtDetail.deuda_total,
+    };
+  }
+}
