31f80143c795847b483c2f8be99d3cf109282ff2
Alan Varde <alan.varde@gmail.com>
2025-12-22 02:15:22 -0300

    fix: save medical history after updating its current treatment

--- apps/api/src/modules/treatments/treatment.service.ts
+++ apps/api/src/modules/treatments/treatment.service.ts
@@ -1,6 +1,7 @@
 import { parseDateFromString } from '@common/utils/date.utils';
 import { PunctureRecord } from '@modules/laboratory/entities/puncture-record.entity';
 import { MedicalHistory } from '@modules/medical-history/entities/medical-history.entity';
+import { MedicalHistoryService } from '@modules/medical-history/services/medical-history.service';
 import { MedicalOrder } from '@modules/medical-orders/entities/medical-order.entity';
 import { PaymentsService } from '@modules/payments/payments.service';
 import { Injectable, NotFoundException } from '@nestjs/common';
@@ -32,6 +33,7 @@ export class TreatmentService {
     @InjectRepository(PunctureRecord)
     private readonly punctureRepo: Repository<PunctureRecord>,
     private readonly paymentsService: PaymentsService,
+    private readonly medicalHistoryService: MedicalHistoryService,
   ) {}
 
   async createTreatment(
@@ -52,6 +54,7 @@ export class TreatmentService {
     });
     const saved = await this.treatmentRepo.save(treatment);
     medicalHistory.currentTreatment = saved;
+    await this.medicalHistoryService.save(medicalHistory);
 
     await this.paymentsService.registerPaymentOrder(
       saved.id,
